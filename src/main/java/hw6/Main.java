a52949a98011bd387ee4aa6ddd11d3cd92bdabb7
D.Plotnikov <plotnikdima5296@gmail.com>
2022-04-01 15:28:34 +0500

    hw6

--- src/main/java/hw6/Main.java
+++ src/main/java/hw6/Main.java
@@ -1,7 +1,16 @@
 package hw6;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("loh");
+        String str = "abcde";
+        String newStr = "";
+        char[] chars = str.toCharArray();
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            newStr += chars[i];
+        }
+        System.out.println(newStr);
     }
 }
